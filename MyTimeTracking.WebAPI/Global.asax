ed6bb08258fa06d6475c0bdfe45460d9f1b5d87f
tenghui <457699987@qq.com>
2017-03-25 22:03:32 +0800

    + Construct Architecture

---
+++ MyTimeTracking.WebAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyTimeTracking.WebAPI.MvcApplication" Language="C#" %>
---
+++ MyTimeTracking.WebAPI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyTimeTracking.WebAPI.Startup))]
+namespace MyTimeTracking.WebAPI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

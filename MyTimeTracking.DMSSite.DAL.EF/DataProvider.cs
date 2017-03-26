74abf8e0ce500f3270e39603954f1c6c626274fc
tenghui <457699987@qq.com>
2017-03-26 22:33:01 +0800

    ~temp

---
+++ MyTimeTracking.DMSSite.DAL.EF/DataProvider.cs
@@ -0,0 +1,24 @@
+﻿using MyTimeTracking.Data.EF.DataContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTimeTracking.DMSSite.DAL.EF
+{
+    public class DataProvider
+    {
+        private readonly MyTimeTrackingDataContext dbcontext;
+
+        public DataProvider(MyTimeTrackingDataContext context)
+        {
+            dbcontext = context;
+        }
+
+        public void Save()
+        {
+            dbcontext.SaveChanges();
+        }
+    }
+}
